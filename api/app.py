46ff15610a86681fe8fa436c205778ac12a2aa0d
Akito_macbookpro <akito.kawamura0310@gmail.com>
2022-03-24 00:20:02 +0900

    add test json

--- api/app.py
+++ api/app.py
@@ -1,12 +1,18 @@
 from flask import Flask, jsonify
+import json
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["Get"])
+@app.route("/", methods=["GET"])
 def health():
     return jsonify({"health": "ok"})
 
+@app.route("/user", methods=["GET"])
+def user():
+    f = open('json_mock/user.json', 'r')
+    result = json.load(f)
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
