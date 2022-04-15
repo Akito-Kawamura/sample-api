e3c84a6929d35517e330fbddd85af9a6f3672eaf
Akito_macbookpro <akito.kawamura0310@gmail.com>
2022-04-15 23:35:30 +0900

    fix confrict

--- api/app.py
+++ api/app.py
@@ -2,7 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/")  # methodとendpointの指定
+@app.get('/') # methodとendpointの指定
 async def hello():
     return {"text": "hello world!"}
+
+class Data(BaseModel):
+    """request data用の型ヒントがされたクラス"""
+    string: str
+    default_none: Optional[int] = None
+    lists: List[int]
+@app.post('/post')
+async def declare_request_body(data: Data):
+    return {"text": f"hello, {data.string}, {data.default_none}, {data.lists}"}
