13ad26e94f2ec7793a682a0c38b0102823d4797b
Akito_macbookpro <akito.kawamura0310@gmail.com>
2022-04-15 23:34:18 +0900

    make route

--- api/app.py
+++ api/app.py
@@ -1,18 +1,8 @@
-from flask import Flask, jsonify
-import json
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
 
-@app.route("/", methods=["GET"])
-def health():
-    return jsonify({"health": "ok"})
-
-@app.route("/user", methods=["GET"])
-def user():
-    f = open('json_mock/user.json', 'r')
-    result = json.load(f)
-    return jsonify(result)
-
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+@app.get("/")  # methodとendpointの指定
+async def hello():
+    return {"text": "hello world!"}
