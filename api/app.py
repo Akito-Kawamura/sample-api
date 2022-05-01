d30eb7b0d3e962573be3907cb6b9b9e00ef651c8
Akito_macbookpro <akito.kawamura0310@gmail.com>
2022-05-01 14:35:11 +0900

    test

--- api/app.py
+++ api/app.py
@@ -6,11 +6,12 @@ app = FastAPI()
 async def hello():
     return {"text": "hello world!"}
 
-class Data(BaseModel):
-    """request data用の型ヒントがされたクラス"""
-    string: str
-    default_none: Optional[int] = None
-    lists: List[int]
-@app.post('/post')
-async def declare_request_body(data: Data):
-    return {"text": f"hello, {data.string}, {data.default_none}, {data.lists}"}
+# class Data(BaseModel):
+#     """request data用の型ヒントがされたクラス"""
+#     string: str
+#     default_none: Optional[int] = None
+#     lists: List[int]
+
+# @app.post('/post')
+# async def declare_request_body(data: Data):
+#     return {"text": f"hello, {data.string}, {data.default_none}, {data.lists}"}
