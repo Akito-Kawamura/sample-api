5615d93df72b360f2cb4b6dc36568f1b831c35c6
Akito_macbookpro <akito.kawamura0310@gmail.com>
2023-01-05 23:49:53 +0900

    add path

--- api/main.py
+++ api/main.py
@@ -1,16 +1,34 @@
+from enum import Enum
+
 from fastapi import FastAPI
+from typing import Dict
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
+async def root() -> Dict[str, str]:
+    return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+async def read_item(item_id: int) -> Dict[str, int]:
+    return {"item_id": item_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
 
 @app.post("/prefecture")
 def read_root(*args):
