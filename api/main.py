3712e374c7106af7786ebcf5bb23c56e11c00f52
Akito_macbookpro <akito.kawamura0310@gmail.com>
2023-01-06 00:04:09 +0900

    add Post

--- api/main.py
+++ api/main.py
@@ -1,13 +1,18 @@
 from enum import Enum
-
 from fastapi import FastAPI
-from typing import Dict
+from typing import Dict, Union
+from pydantic import BaseModel
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
 
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
 
 app = FastAPI()
 
@@ -16,9 +21,9 @@ app = FastAPI()
 async def root() -> Dict[str, str]:
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int) -> Dict[str, int]:
-    return {"item_id": item_id}
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
@@ -29,6 +34,14 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+@app.get("/item/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
 
 @app.post("/prefecture")
 def read_root(*args):
